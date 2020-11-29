233bce0582bfcaf22d52af567de1a7deb1a1cb98
Snigdha_13 <65667637+Snigdha-Pothuri@users.noreply.github.com>
2020-11-29 11:27:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,28 +1,47 @@
-var form,player,game;
-var database,position;
-var gameState=0; 
-var playerCount ;
-var allplayers;
+var pencil = createSprite(200,200,35,35);
+var red = createSprite(50,50,35,35);
+var blue = createSprite(125,50,35,35);
+var yellow = createSprite(200,50,35,35);
+var pink = createSprite(275,50,35,35); 
+var reset = createSprite(350,50,35,35);
+
+red.shapeColor="red";
+blue.shapeColor="blue";
+yellow.shapeColor="yellow";
+pink.shapeColor="pink";
 
 
-function setup(){
-    database= firebase.database();
-    createCanvas(displayWidth,displayHeight-200);
-    game= new Game() 
-    game.getState()
-    game.start()
-    
+
+
+
+function draw() { 
+  
+pencil.x= mouseX
+pencil.y=mouseY; 
+
+if (mousePressedOver(red)) {
+ pencil.shapeColor="red" ; 
  }
-
-function draw(){
-
-   if (playerCount===4){
-    game.update(1);   
-   }
-   if (gameState===1){
-clear();
-game.play(); 
-}  
-
+if (mousePressedOver(blue)) {
+pencil.shapeColor="blue" ; 
+}
+if (mousePressedOver(yellow)) {
+pencil.shapeColor="yellow";  
+} 
+if (mousePressedOver(pink)) {
+pencil.shapeColor="pink";  
+}
+if (mousePressedOver(reset)) {
+background("white");  
 }
 
+if (pencil.y<65) {
+pencil.visible=false;  
+}
+if (pencil.y>65) {
+pencil.visible=true;  
+}
+
+  
+drawSprites();  
+}
